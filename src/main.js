4eac7830c451076d8a703825fd653336f8a5c0d7
angl-t <no1rrav3n@gmail.com>
2025-10-28 10:24:30 -0700

    Added in firestore collections & modified main.js, firebaseConfig.js, & authentication.js (as well as main.html).

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import {
     onAuthReady
 } from "./authentication.js"
+import { db } from "./firebaseConfig.js";
+import { doc, onSnapshot } from "firebase/firestore";
+
+// Function to read the quote of the day from Firestore
+function readQuote(day) {
+    const quoteDocRef = doc(db, "quotes", day); // Get a reference to the document
+
+    onSnapshot(quoteDocRef, (docSnap) => { // Listen for real-time updates
+        if (docSnap.exists()) {
+            document.getElementById("quote-goes-here").innerHTML = docSnap.data().quote;
+        } else {
+            console.log("No such document!");
+        }
+    }, (error) => {
+        console.error("Error listening to document: ", error);
+    });
+}
+
+readQuote("tuesday");
 
 function showDashboard() {
       const nameElement = document.getElementById("name-goes-here"); // the <h1> element to display "Hello, {name}"
